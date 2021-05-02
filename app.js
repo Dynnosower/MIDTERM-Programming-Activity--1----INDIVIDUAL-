ad338e9e7d6015c080de5479ce9b3c09b894414e
Dynnosower <infantevillarin@gmail.com>
2021-05-03 00:22:42 +0800

    Version 1

---
+++ app.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict'
+angular.module('MsgApp', [])
+.controller('MsgController', MsgController);
+
+MsgController.$inject = ['$scope'];
+function MsgController($scope){
+    $scope.name = "Yaakov";
+}
+})();
